7008b02d984715a6cb3401a60e5abf073e95958e
Steven Thonus <steven.thonus@ippz.nl>
2016-02-19 16:57:28 +0100

    use session instead of appstate

--- src/cljs/prob/core.cljs
+++ src/cljs/prob/core.cljs
@@ -10,35 +10,9 @@
 
 (enable-console-print!)
 
-;; -------------------------
-;; State
-(def app-state (r/atom {}))
 
 ;; -------------------------
-;; Routes
-(secretary/set-config! :prefix "#")
-(defroute "/" []
-  (swap! app-state assoc :page :home))
-(defroute "/highscores" []
-  (swap! app-state assoc :page :highscores))
-(defroute "/start" []
-  (swap! app-state assoc :page :start))
-(defroute "/stop" []
-  (swap! app-state assoc :page :stop))
-
-;; -------------------------
-;; History
-;; must be called after routes have been defined
-(defn hook-browser-navigation! []
-  (doto (History.)
-    (events/listen
-     EventType/NAVIGATE
-     (fn [event]
-       (secretary/dispatch! (.-token event))))
-    (.setEnabled true)))
-
-;; -------------------------
-;; Pages
+;; Views
 (defn menu-component []
   [:div.row
    [:a.col-xs-3.col-md-2.btn.btn-default {:role "button"
@@ -55,11 +29,10 @@
     [:i.fa.fa-twitter] " Home"]])
 
 (defn home-page []
-  (let [page (get @app-state :page)]
-    [:div
-     [:h2 "Home"]
-     (menu-component)
-     [:p (str "page: " page)]]))
+  [:div
+   [:h2 "Home"]
+   (menu-component)
+   [:p "home!"]])
 
 (defn highscores-page []
   [:div
@@ -76,19 +49,34 @@
    [:h2 "Stop"]
    (menu-component)])
 
+(defn current-page []
+  [:div [(session/get :current-page)]])
+
+
 ;; -------------------------
-;; App-state
-(defmulti current-page #(@app-state :page))
-(defmethod current-page :home []
-  [home-page])
-(defmethod current-page :highscores []
-  [highscores-page])
-(defmethod current-page :start []
-  [start-page])
-(defmethod current-page :stop []
-  [stop-page])
-(defmethod current-page :default []
-  [home-page])
+;; Routes
+(secretary/set-config! :prefix "#")
+(defroute "/" []
+  (session/put! :current-page #'home-page))
+(defroute "/highscores" []
+  (session/put! :current-page #'highscores-page))
+(defroute "/start" []
+  (session/put! :current-page #'start-page))
+(defroute "/stop" []
+  (session/put! :current-page #'stop-page))
+
+
+;; -------------------------
+;; History
+;; must be called after routes have been defined
+(defn hook-browser-navigation! []
+  (doto (History.)
+    (events/listen
+     EventType/NAVIGATE
+     (fn [event]
+       (secretary/dispatch! (.-token event))))
+    (.setEnabled true)))
+
 
 ;; -------------------------
 ;; Initialize app
